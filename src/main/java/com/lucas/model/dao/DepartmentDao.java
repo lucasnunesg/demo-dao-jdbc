861ccc10e7fc2a58cee6870562294c7f5d9a93f8
Lucas Nunes <lucas.nunesgomes1@gmail.com>
2024-01-22 12:29:52 -0300

    implemented `DepartmentDao` and `SellerDao` interfaces

---
+++ src/main/java/com/lucas/model/dao/DepartmentDao.java
@@ -0,0 +1,15 @@
+package com.lucas.model.dao;
+
+import com.lucas.model.entities.Department;
+
+import java.util.List;
+
+public interface DepartmentDao {
+
+    void insert(Department obj);
+    void update(Department obj);
+    void deleteById(Integer id);
+    Department findById(Integer id);
+    List<Department> findAll();
+
+}
