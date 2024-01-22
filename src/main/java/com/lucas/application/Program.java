951833b7f23edf4003214e906ed99adcbcceb1dc
Lucas Nunes <lucas.nunesgomes1@gmail.com>
2024-01-22 12:25:06 -0300

    created `Seller` class

--- src/main/java/com/lucas/application/Program.java
+++ src/main/java/com/lucas/application/Program.java
@@ -1,10 +1,17 @@
 package com.lucas.application;
 
 import com.lucas.model.entities.Department;
+import com.lucas.model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
+
         Department obj = new Department(1, "Books");
         System.out.println(obj);
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+        System.out.println(seller);
     }
 }
